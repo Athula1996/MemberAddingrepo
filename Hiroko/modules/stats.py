3fe659fc1402f389127a380182c4a3a1271d4a0b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-01 17:14:26 +0530

    Update stats.py

--- Hiroko/modules/stats.py
+++ Hiroko/modules/stats.py
@@ -1,9 +1,9 @@
 from pyrogram import filters, Client
 from pyrogram.types import Message
 from config import OWNER_ID
-from Zebra import Zebra
-from Zebra.Helper.db.chats import get_served_chats
-from Zebra.Helper.db.users import get_served_users
+from Hiroko import Hiroko
+from Hiroko.Helper.database.chats import get_served_chats
+from Hiroko.Helper.database.users import get_served_users
 
 # --------------------------------------------------------------------------------- #
 
