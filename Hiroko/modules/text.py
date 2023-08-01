12845cc81d272cd84f65d15018388e1508904147
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-02 00:45:03 +0530

    Update text.py

--- Hiroko/modules/text.py
+++ Hiroko/modules/text.py
@@ -11,15 +11,23 @@ SHORTENER_TEXT = """
 ᴀɴᴅ ɪᴛ ᴡɪʟʟ ᴄᴏɴᴠᴇʀᴛ ɪᴛ ɪɴᴛᴏ ᴀ ꜱʜᴏʀᴛᴇɴᴇᴅ ʟɪɴᴋ ꜰᴏʀ ʏᴏᴜ**
 """
 
+
 ADMINS_TEXT = """
 » <code>/zombies</code> : **ʀᴇᴍᴏᴠᴇs ᴛʜᴇ ᴅᴇʟᴇᴛᴇᴅ ᴀᴄᴄᴏᴜɴᴛs ғʀᴏᴍ ᴛʜᴇ ɢʀᴏᴜᴘ.**
+
 » <code>/pin</code> : **ᴘɪɴs ᴛʜᴇ ᴍᴇssᴀɢᴇ ʀᴇᴘʟɪᴇᴅ ᴛᴏ - ᴀᴅᴅ 'ʟᴏᴜᴅ' ᴏʀ 'ɴᴏᴛɪғʏ' ᴛᴏ ɢɪᴠᴇ ɴᴏᴛɪғʏ ᴛᴏ ᴜsᴇʀs.**
+
 » <code>/unpin</code> : **ᴜɴᴘɪɴs ᴛʜᴇ ᴄᴜʀʀᴇɴᴛʟʏ ᴘɪɴɴᴇᴅ ᴍᴇssᴀɢᴇ.**
 
+» <code>/purge</code> :  **ᴅᴇʟᴇᴛᴇs ᴀʟʟ ᴍᴇssᴀɢᴇs ʙᴇᴛᴡᴇᴇɴ ᴛʜɪs ᴀɴᴅ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴛᴏ ᴍᴇssᴀɢᴇs.**
+
 » <code>/setphoto</code> : **sᴇᴛs ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ɪᴍᴀɢᴇ/ᴅᴏᴄᴜᴍᴇɴᴛ ᴀs ᴛʜᴇ ɢʀᴏᴜᴘ ᴘɪᴄᴛᴜʀᴇ.**
+
 » <code>/removephoto</code> : **ᴅᴇʟᴇᴛᴇs ᴛʜᴇ ɢʀᴏᴜᴘ ᴘɪᴄᴛᴜʀᴇ.**
+
 » <code>/settitle</code> : **sᴇᴛs ᴛʜᴇ ɢʀᴏᴜᴘ ᴛɪᴛᴇʟ**
 """
+
 
 GITHUB_TEXT = """
 ᴘʀᴏᴠɪᴅᴇs ʏᴏᴜ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴀ ɢɪᴛʜᴜʙ ᴘʀᴏғɪʟᴇ
@@ -31,14 +39,52 @@ GITHUB_TEXT = """
 FUN_TEXT = """
 **ʜᴇʀᴇ ᴀʀᴇ sᴏᴍᴇ ғᴜɴ ᴄᴏᴍᴍᴀɴᴅs.**
 » <code>/dice</code> :
+
 » <code>/arrow</code> :
+
 » <code>/goal</code> :
+
 » <code>/luck</code> :
+
 » <code>/throw</code> :
+
 » <code>/bowling</code> :
-» <code>/react</code> or <code>/reaction</code> :
-» <code>/aq</code> or <code>/animequotes</code> :
+
+» <code>/react</code> or <code>/reaction</code> : **ʀᴇᴀᴄᴛ ᴡɪᴛʜ ᴀ ʀᴀɴᴅᴏᴍ ʀᴇᴀᴄᴛɪᴏɴ.** 
+
+» <code>/aq</code> or <code>/animequotes</code> : **sᴇɴᴅ ᴀ ʀᴀɴᴅᴏᴍ ᴀɴɪᴍᴇ ǫᴜᴏᴛᴇs.**
 """
 
 
+MISC_TEXT = """
+» /id: **ɢᴇᴛ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ɢʀᴏᴜᴘ ɪᴅ. ɪғ ᴜsᴇᴅ ʙʏ ʀᴇᴘʟʏɪɴɢ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ, ɢᴇᴛs ᴛʜᴀᴛ ᴜsᴇʀ's ɪᴅ.**
 
+» /info: **ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴀ ᴜsᴇʀ.**
+
+» /tr : **ᴛʀᴀɴsʟᴀᴛᴇ ᴛᴇxᴛ ᴡʀɪᴛᴛᴇɴ ᴏʀ ʀᴇᴘʟʏ ғᴏʀ ᴀɴʏ ʟᴀɴɢᴜᴀɢᴇ ᴛᴏ ᴛʜᴇ ɪɴᴛᴇɴᴇᴅ ʟᴀɴɢᴜᴀɢᴇ.**
+
+» /wiki : **sᴇᴀʀᴄʜ ғᴏʀ ᴛᴇxᴛ ᴡʀɪᴛᴛᴇɴ ғʀᴏᴍ ᴛʜᴇ ᴡɪᴋɪᴘᴇᴅɪᴀ sᴏᴜʀᴄᴇ.**
+
+» /video 
+
+» /song
+
+» /tg or /telegraph
+
+» /speedtest
+
+» /admins or /staff : list of admins in the chat
+
+» /bots
+
+"""
+
+
+NEKOS_TEXT = """
+
+"""
+
+
+GAMES_TEXT = """
+
+"""
