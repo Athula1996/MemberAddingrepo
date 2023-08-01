64ebe6cb674522a9a876c99006ab172a803dadbf
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-01 20:21:20 +0530

    Update stats.py

--- Hiroko/modules/stats.py
+++ Hiroko/modules/stats.py
@@ -7,7 +7,7 @@ from Hiroko.Helper.database.users import get_served_users
 
 # --------------------------------------------------------------------------------- #
 
-@Zebra.on_message(filters.command("stats") & filters.user(OWNER_ID))
+@Hiroko.on_message(filters.command("stats") & filters.user(OWNER_ID))
 async def stats(cli: Client, message: Message):
     users = len(await get_served_users())
     chats = len(await get_served_chats())
