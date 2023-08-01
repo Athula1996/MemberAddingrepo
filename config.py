ea75fb73f3133e9e6a2cedce8b2964d8e010e7f6
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-01 19:05:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_USERNAME = getenv("BOT_USERNAME", "@HirokoRobot")
+BOT_USERNAME = getenv("BOT_USERNAME", "HirokoRobot")
 COMMAND_HANDLER = ["/", "!"]
 BOT_TOKEN = getenv("BOT_TOKEN", "6552685718:AAFNWz5Rk7CBeqLYT2c2ty868wW280ygm7o")
 OWNER_ID = int(getenv("OWNER_ID", "6109551937"))
