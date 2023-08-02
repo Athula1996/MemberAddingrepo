c88b9366463c85de21a72ae8e520802d05166ca2
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-03 00:56:54 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -5,35 +5,35 @@ fileopen = open("Hiroko\Helper\database\api.text","r")
 API = fileopen.read()
 fileopen.close()
 
-
-
 openai.api_key = API
 load_dotenv()
 completion = openai.Completion()
 
-
-def ReplyBrain (question, chat_log= None):
-    FileLog = open("","r")
+def ReplyBrain(question, chat_log=None):
+    FileLog = open("", "r")
     chat_log_template = FileLog.read()
     FileLog.close()
 
-    if chat log is None:
-    chat_log= chat_log_template
+    if chat_log is None:
+        chat_log = chat_log_template
 
-
-    prompt = f(chat_log) You: (question}\nHiroko :
-    response = completion.create( model = "text-davinci-882",
-        prompt prompt,
-        temperature = 0.5,
-        max_tokens = 60,
-        top_p = 0.3,
-        frequency penalty = 0.5,
-        presence_penalty = 0)
+    prompt = f"{chat_log}You: {question}\nHiroko:"
+    response = completion.create(
+        model="text-davinci-882",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=60,
+        top_p=0.3,
+        frequency_penalty=0.5,
+        presence_penalty=0
+    )
     answer = response.choices[0].text.strip()
-    chat_log_template_update = chat_log_template + "\nYou : {question) \nHiroko : {answer}"
-    FileLog = open("","w")
+    chat_log_template_update = chat_log_template + f"\nYou: {question}\nHiroko: {answer}"
+    FileLog = open("", "w")
     FileLog.write(chat_log_template_update)
     FileLog.close()
     return answer
 
- 
+
+
+
