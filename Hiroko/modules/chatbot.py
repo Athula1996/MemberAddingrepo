c234085a450abc2e31dfdc56aa6eed150a3af62f
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-03 07:48:53 +0530

    Update chatbot.py

--- Hiroko/modules/chatbot.py
+++ Hiroko/modules/chatbot.py
@@ -1,3 +1,4 @@
+
 import openai
 from Hiroko import Hiroko
 from pyrogram import Client
@@ -5,11 +6,7 @@ from pyrogram.raw.functions.channels import GetFullChannel, GetMessages
 
 
 API_KEY = "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"
-
 CHANNEL_ID = -1001915298220  
-
-
-
 
 @Hiroko.on_message()
 def reply_brain(client, message):
@@ -17,7 +14,7 @@ def reply_brain(client, message):
         question = message.text
         
         # Get previous chat log from the channel
-        chat_log = get_chat_log(client)
+        chat_log = get_chat_log(app)
 
         answer = generate_response(question, chat_log)
         update_chat_log(chat_log, question, answer)
@@ -27,7 +24,7 @@ def reply_brain(client, message):
 
 def get_chat_log(client):
     # Get channel full info to access the channel chat log
-    channel_info = client.send(GetFullChannel(channel=CHANNEL_ID))
+    channel_info = app.send(GetFullChannel(channel=CHANNEL_ID))
     chat_log_message_id = channel_info.full_chat.about.id
 
     messages = client.send(GetMessages(channel=CHANNEL_ID, id=[chat_log_message_id]))
@@ -42,7 +39,6 @@ def get_chat_log(client):
 
 def generate_response(question, chat_log):
     # Use the ChatGPT API to generate a response
-    import openai
     openai.api_key = API_KEY
     completion = openai.Completion()
 
@@ -60,13 +56,9 @@ def generate_response(question, chat_log):
 
     return answer
 
-
 def update_chat_log(chat_log, question, answer):
     chat_log_template_update = f"{chat_log}\nYou: {question}\nHiroko: {answer}"
     
     # Update the chat log in the channel
-    app.send_message(CHANNEL_ID, chat_log_template_update)
-
-
-
+    Hiroko.send_message(CHANNEL_ID, chat_log_template_update)
 
