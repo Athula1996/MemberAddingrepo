b3f53cc2b05086e10eaf8756bad9683819898f82
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-08 09:28:54 +0530

    Update welcome.py

--- Hiroko/modules/welcome.py
+++ Hiroko/modules/welcome.py
@@ -6,23 +6,16 @@ from pyrogram import Client, filters
 from Hiroko import Hiroko
 
 
-get_font = lambda font_size, font_path: ImageFont.truetype(font_path, font_size)
-resize_text = (
-    lambda text_size, text: (text[:text_size] + "...").upper()
-    if len(text) > text_size
-    else text.upper()
-)
+bg_path = "./Hiroko/Helper/assests/thumbnail.png"
+font_path = "./Hiroko/Helper/assests/Hiroko.ttf"
+WELCOME_TEXT = """
+ᴡᴇʟᴄᴏᴍᴇ {name}
 
+ᴛʜᴀɴᴋs ғᴏʀ ᴊᴏɪɴɪɴɢ ᴏᴜʀ **{chat_title}** !
+"""
+DEL_AFTER_WELCOME = True
 
-async def get_welcome_img(
-    bg_path: str,
-    font_path: str,
-    user_id: int | str,
-    name: str,
-    username: str,
-    chat_name: str,
-    profile_path: str = None,
-):
+async def get_welcome_img(bg_path: str, font_path: str, user_id: int | str, name: str, username: str, chat_name: str, profile_path: str = None):
     bg = Image.open(bg_path)
     if profile_path:
         img = Image.open(profile_path)
@@ -37,7 +30,6 @@ async def get_welcome_img(
 
     img_draw = ImageDraw.Draw(bg)
 
-        
     img_draw.text(
         (890, 595),
         text=str(user_id).upper(),
@@ -52,61 +44,65 @@ async def get_welcome_img(
         fill=(275, 275, 275),
     )
 
-
     path = f"./Welcome_img_{user_id}.png"
     bg.save(path)
     return path
 
 
-bg_path = "./Hiroko/Helper/assests/thumbnail.png"
-font_path = "./Hiroko/Helper/assests/Hiroko.ttf"
-
-WELCOME_TEXT = """
-ᴡᴇʟᴄᴏᴍᴇ {name}
-
-ᴛʜᴀɴᴋs ғᴏʀ ᴊᴏɪɴɪɴɢ ᴏᴜʀ **{chat_title}!**
-"""
 
 
-DEL_AFTER_WELCOME = True  
+async def download_profile_photo(client, photo_id):
+    try:
+        profile = await client.download_media(photo_id)
+    except AttributeError:
+        profile = None
+    return profile
+
 
 @Hiroko.on_message(filters.new_chat_members, group=3)
-async def _greet(client, message):
+async def greet_users(client, message):
     chat = message.chat
     my_id = client.me.id
+    tasks = []
+    
     for member in message.new_chat_members:
         user_id = member.id
         username = member.username
         name = member.first_name if member.first_name else member.last_name
-        try:
-            profile = await Hiroko.download_media(member.photo.big_file_id)
-        except AttributeError:
-            profile = None
         if user_id == my_id:
-            return
+            continue
+        
+        tasks.append(download_profile_photo(client, member.photo.big_file_id))
+    
+    profile_paths = await asyncio.gather(*tasks)
+    
+    for member, profile_path in zip(message.new_chat_members, profile_paths):
+        user_id = member.id
+        username = member.username
+        name = member.first_name if member.first_name else member.last_name
         welcome_photo = await get_welcome_img(
             bg_path=bg_path,
             font_path=font_path,
             user_id=user_id,
             name=name,
             username=username,
-            chat_name=chat.title,  # Replace chat_title with chat.title
-            profile_path=profile,
+            chat_name=chat.title,
+            profile_path=profile_path,
         )
         welcome_caption = WELCOME_TEXT.format(
             chat_title=chat.title, name=name, username=username, user_id=user_id
         )
-        msg = await Hiroko.send_photo(
+        async with await Hiroko.send_photo(
             chat_id=chat.id, photo=welcome_photo, caption=welcome_caption
-        )
+        ) as msg:
+            if DEL_AFTER_WELCOME:
+                async def del_welcome_pic():
+                    await asyncio.sleep(10 * 60)
+                    await msg.delete()
+
+                loop = asyncio.get_running_loop()
+                loop.create_task(del_welcome_pic())
+
         Path(welcome_photo).unlink(missing_ok=True)
-        if DEL_AFTER_WELCOME:
-            async def del_welcome_pic():
-                await asyncio.sleep(10 * 60)
-                await msg.delete()
-
-            loop = asyncio.get_running_loop()
-            loop.create_task(del_welcome_pic())
-
 
 
