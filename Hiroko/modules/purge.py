57c8cbf8bb09fec9768e9c2a0656779fe0c16bf2
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-08 22:19:24 +0530

    Update purge.py

--- Hiroko/modules/purge.py
+++ Hiroko/modules/purge.py
@@ -42,3 +42,25 @@ async def purge(client, message):
     await status_message.delete()
     
 # ------------------------------------------------------------------------------- #
+
+@Hiroko.on_message(filters.command("del")& admin_filter)
+async def _del(_, message):
+    if message.sender_chat:
+        return
+    replied = message.reply_to_message
+    chat_id = message.chat.id
+    if not replied:
+        return await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴅᴇʟᴇᴛᴇ ɪᴛ.**")
+
+    try:
+        await Hiroko.delete_messages(chat_id, replied.id)
+        await message.delete()
+    except:
+        pass
+
+
+# ------------------------------------------------------------------------------- #
+
+
+
+        
