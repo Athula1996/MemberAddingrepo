86847b15116a9c0d9eace9b664df851bd0bd5fae
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-10 00:55:46 +0530

    Update clone.py

--- Hiroko/modules/clone.py
+++ Hiroko/modules/clone.py
@@ -70,19 +70,3 @@ async def delete_token(bot, msg: Message):
 
 
 
-@Hiroko.on_message(filters.private & filters.command("bclone"))
-async def clone(bot, msg: Message):
-    chat = msg.chat
-    text = await msg.reply("Usage:\n\n /clone token")
-    cmd = msg.command
-    phone = msg.command[1]
-    try:
-        await text.edit("Booting Your Client")
-                   # change this Directry according to ur repo
-        client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "Hiroko.modules"})
-        await client.start()
-        user = await client.get_me()
-        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot\n\nThanks for Cloning.")
-    except Exception as e:
-        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
-
