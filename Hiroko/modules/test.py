58d48c5eb0c619cbded24b531e575e9bfcf1f334
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-10 15:19:38 +0530

    Update test.py

--- Hiroko/modules/test.py
+++ Hiroko/modules/test.py
@@ -111,7 +111,7 @@ async def userstatus(user_id):
 # --------------------------------------------------------------------------------- #
 
 
-@Hiroko.on_message(filters.command(["info","userinfo"]))
+@Hiroko.on_message(filters.command(["pinfo","puserinfo"]))
 async def userinfo(_, message):
     
      chat_id = message.chat.id
