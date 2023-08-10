839e7603da845cf54779e4c1ee14492dd18de20d
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-10 14:19:51 +0530

    Update misc.py

--- Hiroko/modules/misc.py
+++ Hiroko/modules/misc.py
@@ -1,10 +1,13 @@
 import time
-from asyncio import sleep
 import aiohttp
+import wikipedia
+from asyncio import sleep
 from Hiroko import Hiroko
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message
+
+
 
 
 # --------------------------------------------------------------------------------- #
@@ -293,6 +296,38 @@ def calculate_math(client, message):
     message.reply(response)
 
 
-# --------------------------------------------------------------------------------- #
+# ------------------------------------------------------------------------------- #
+
+@Hiroko.on_message(filters.command("wiki"))
+async def wikipediasearch(_, message):
+    reply = message.reply_to_message
+    if len(message.command) < 2:
+          await message.reply_text("**ᴇxᴀᴍᴘʟᴇ**:\n`/wiki telegram`")
+          return 
+    query = (
+        message.text.split(None, 1)[1]
+        if len(message.command) < 3
+        else message.text.split(None, 1)[1].replace(" ", "%20")
+    )
+    if not query:
+        await message.reply_text("**ɪɴᴠᴀʟɪᴅ sʏɴᴛᴀx sᴇᴇ ʜᴇʟᴘ ᴍᴇɴᴜ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.**")
+        return
+    msg = await message.reply_text("**sᴇᴀʀᴄʜɪɴɢ...**")
+    results = wikipedia.search(query)
+    result = ""
+    for s in results:
+        try:
+            page = wikipedia.page(s)
+            url = page.url
+            result += f"𒊹︎︎︎[{s}]({url}) \n"
+        except BaseException:
+            pass
+    await message.reply_text(
+        "**ᴡɪᴋɪᴘᴇᴅɪᴀ sᴇᴀʀᴄʜ: {}** \n\n**ʀᴇsᴜʟᴛ:**\n{}".format(query, result), disable_web_page_preview=True)
+    await msg.delete()
+
+
+# ------------------------------------------------------------------------------- #
+
 
 
