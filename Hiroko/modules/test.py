90daec1e1b4280312342faa6b7c43bb0d254c083
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-08-10 15:16:24 +0530

    Update test.py

--- Hiroko/modules/test.py
+++ Hiroko/modules/test.py
@@ -2,8 +2,16 @@ import asyncio
 import os
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
-from pyrogram import Client, filters
 from Hiroko import Hiroko
+import time
+import aiohttp
+from asyncio import sleep
+from pyrogram import filters, Client, enums
+from pyrogram.enums import ParseMode
+from pyrogram.types import Message
+
+
+
 
 
 get_font = lambda font_size, font_path: ImageFont.truetype(font_path, font_size)
@@ -47,7 +55,7 @@ async def get_welcome_img(
 
     img_draw.text(
         (180, 340),
-        text=resize_text(60, chat_name),
+        text=resize_text(60, name),
         font=get_font(100, font_path),
         fill=(275, 275, 275),
     )
@@ -61,49 +69,124 @@ async def get_welcome_img(
 bg_path = "./Hiroko/Helper/assests/thumbnail.png"
 font_path = "./Hiroko/Helper/assests/Hiroko.ttf"
 
-WELCOME_TEXT = """
-ᴡᴇʟᴄᴏᴍᴇ {name}
+  
 
-ᴛʜᴀɴᴋs ғᴏʀ ᴊᴏɪɴɪɴɢ ᴏᴜʀ **{chat_title}!**
+
+# --------------------------------------------------------------------------------- #
+
+INFO_TEXT = """
+**ᴜsᴇʀ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ**:
+
+**ᴜsᴇʀ ɪᴅ:** `{}`
+
+**ɴᴀᴍᴇ:** {}
+**ᴜsᴇʀɴᴀᴍᴇ: @{}
+**ᴍᴇɴᴛɪᴏɴ:** {}
+
+**ᴜsᴇʀ sᴛᴀᴛᴜs:**\n`{}`\n
+**ᴅᴄ ɪᴅ:** {}
+**ʙɪᴏ:** {}
 """
 
+# --------------------------------------------------------------------------------- #
 
-DEL_AFTER_WELCOME = True  
+async def userstatus(user_id):
+   try:
+      user = await Hiroko.get_users(user_id)
+      x = user.status
+      if x == enums.UserStatus.RECENTLY:
+         return "User was seen recently."
+      elif x == enums.UserStatus.LAST_WEEK:
+          return "User was seen last week."
+      elif x == enums.UserStatus.LONG_AGO:
+          return "User was seen long ago."
+      elif x == enums.UserStatus.OFFLINE:
+          return "User is offline."
+      elif x == enums.UserStatus.ONLINE:
+         return "User is online."
+   except:
+        return "**sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ʜᴀᴘᴘᴇɴᴇᴅ !**"
+    
 
-@Hiroko.on_message(filters.new_chat_members, group=3)
-async def _greet(client, message):
-    chat = message.chat
-    my_id = client.me.id
-    for member in message.new_chat_members:
-        user_id = member.id
-        username = member.username
-        name = member.first_name if member.first_name else member.last_name
-        try:
-            profile = await Hiroko.download_media(member.photo.big_file_id)
-        except AttributeError:
-            profile = None
-        if user_id == my_id:
-            return
-        welcome_photo = await get_welcome_img(
+# --------------------------------------------------------------------------------- #
+
+
+@Hiroko.on_message(filters.command(["info","userinfo"]))
+async def userinfo(_, message):
+    
+     chat_id = message.chat.id
+     user_id = message.from_user.id
+     if not message.reply_to_message and len(message.command) == 2:
+         
+         try:
+            user_id = message.text.split(None, 1)[1]
+            user_info = await Hiroko.get_chat(user_id)
+            user = await Hiroko.get_users(user_id)            
+            status = await userstatus(user.id)
+            id = user_info.id
+            dc_id = user.dc_id
+            name = user_info.first_name
+            username = user_info.username
+            mention = user.mention
+            bio = user_info.bio
+            photo = await Hiroko.download_media(user.photo.big_file_id)
+            welcome_photo = await get_welcome_img(
             bg_path=bg_path,
             font_path=font_path,
-            user_id=user_id,
-            name=name,
-            username=username,
-            chat_name=chat.title,  # Replace chat_title with chat.title
-            profile_path=profile,
-        )
-        welcome_caption = WELCOME_TEXT.format(
-            chat_title=chat.title, name=name, username=username, user_id=user_id
-        )
-        msg = await Hiroko.send_photo(
-            chat_id=chat.id, photo=welcome_photo, caption=welcome_caption
-        )
-        Path(welcome_photo).unlink(missing_ok=True)
-        if DEL_AFTER_WELCOME:
-            async def del_welcome_pic():
-                await asyncio.sleep(10 * 60)
-                await msg.delete()
+            user_id=user_id,                
+            profile_path=photo,
+        ) 
+            await Hiroko.send_photo(chat_id,photo=welcome_photo, caption=INFO_TEXT.format(
+id,name, username, mention, status, dc_id, bio),reply_to_message_id=message.id)
+         except Exception as e:
+              await message.reply_text(str(e))
+    
+     elif not message.reply_to_message:
+         try:
+            user_info = await Hiroko.get_chat(user_id)
+            user = await Hiroko.get_users(user_id)
+            status = await userstatus(user.id)
+            id = user_info.id
+            dc_id = user.dc_id
+            name = user_info.first_name
+            username = user_info.username
+            mention = user.mention
+            bio = user_info.bio
+            photo = await Hiroko.download_media(user.photo.big_file_id)
+            welcome_photo = await get_welcome_img(
+            bg_path=bg_path,
+            font_path=font_path,
+            user_id=user_id,                
+            profile_path=photo,
+        )  
+            await Hiroko.send_photo(chat_id,photo=welcome_photo, caption=INFO_TEXT.format(
+id,name, username, mention,status, dc_id, bio),reply_to_message_id=message.id)
+         except Exception as e:
+              await message.reply_text(str(e))
+     elif message.reply_to_message:
+          user_id = message.reply_to_message.from_user.id
+          try:
+            user_info = await Hiroko.get_chat(user_id)
+            user = await Hiroko.get_users(user_id)
+            status = await userstatus(user.id)
+            id = user_info.id
+            dc_id = user.dc_id        
+            name = user_info.first_name
+            username = user_info.username
+            mention = user.mention
+            bio = user_info.bio
+            photo = await Hiroko.download_media(message.reply_to_message.from_user.photo.big_file_id)
+            welcome_photo = await get_welcome_img(
+            bg_path=bg_path,
+            font_path=font_path,
+            user_id=user_id,                
+            profile_path=photo,
+        )   
+            await Hiroko.send_photo(chat_id,photo=welcome_photo,caption=INFO_TEXT.format(
+id,name, username, mention,status, dc_id, bio),reply_to_message_id=message.id)
+          except Exception as e:
+              await message.reply_text(str(e))
 
-            loop = asyncio.get_running_loop()
-            loop.create_task(del_welcome_pic())
+
+         
+    
